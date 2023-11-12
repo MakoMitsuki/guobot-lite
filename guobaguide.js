7a006785ffb2f5396f2d2960235daae7fcc15d8a
Mika C <makomitsuki@gmail.com>
2023-11-12 15:00:00 +0000

    update guidelist

--- guobaguide.js
+++ guobaguide.js
@@ -6,10 +6,167 @@ charalist = [
         ],
     },
     {
+        charaname: 'baliu',
+        imgFiles: [
+            'bailu-sheet.png',
+        ],
+    },
+    {
+        charaname: 'blade',
+        imgFiles: [
+            'BladeGuobaGraphic.png',
+            'BladeLCGuobaGraphic.png',
+        ],
+    },
+    {
+        charaname: 'bronya',
+        imgFiles: [
+            'BronyaGuobagraphic1.2.png',
+        ],
+    },
+    {
+        charaname: 'clara',
+        imgFiles: [
+            'ClaraGuobagraphic.png',
+        ],
+    },
+    {
+        charaname: 'dan heng',
+        imgFiles: [
+            'DanhengGuobagraphic.png',
+        ],
+    },
+    {
         charaname: 'dhil',
         imgFiles: [
             'DHILGuobaGraphic.png',
             'DHILLCGuobaGraphic.png',
+        ],
+    },
+    {
+        charaname: 'fu xuan',
+        imgFiles: [
+            'FuXuanGuoba.png',
+        ],
+    },
+    {
+        charaname: 'gepard',
+        imgFiles: [
+            'GepardGuobagraphic.png',
+        ],
+    },
+    {
+        charaname: 'guinaifen',
+        imgFiles: [
+            'GuinaifenInfographicGuoba.png',
+        ],
+    },
+    {
+        charaname: 'himeko',
+        imgFiles: [
+            'HimekoGuobaInfographic.png',
+        ],
+    },
+    {
+        charaname: 'Jingliu',
+        imgFiles: [
+            'JingliuGuobaInfographic.png',
+        ],
+    },
+    {
+        charaname: 'kafka',
+        imgFiles: [
+            'KafkaGuobagraphic.png',
+        ],
+    },
+    {
+        charaname: 'luka',
+        imgFiles: [
+            'LukaGuobagraphic.png',
+        ],
+    },
+    {
+        charaname: 'luocha',
+        imgFiles: [
+            'LuochaGuobagraphic.png',
+        ],
+    },
+    {
+        charaname: 'lynx',
+        imgFiles: [
+            'LynxGuoba.png',
+        ],
+    },
+    {
+        charaname: 'pela',
+        imgFiles: [
+            'PelaGuobagraphic.png',
+        ],
+    },
+    {
+        charaname: 'qinque',
+        imgFiles: [
+            'QingqueGuobagraphic.png',
+        ],
+    },
+    {
+        charaname: 'sampo',
+        imgFiles: [
+            'SampoGuobagraphic.png',
+        ],
+    },
+    {
+        charaname: 'seele',
+        imgFiles: [
+            'SeeleInfographicGuoba.png',
+        ],
+    },
+    {
+        charaname: 'serval',
+        imgFiles: [
+            'ServalInfographic.png',
+        ],
+    },
+    {
+        charaname: 'silver wolf',
+        imgFiles: [
+            'SW1.3Guobagraphic.png',
+        ],
+    },
+    {
+        charaname: 'sushang',
+        imgFiles: [
+            'SushangGuobagraphic.png',
+        ],
+    },
+    {
+        charaname: 'tingyun',
+        imgFiles: [
+            'TingyunGuobagraphic.png',
+        ],
+    },
+    {
+        charaname: 'topaz',
+        imgFiles: [
+            'TopazInfographicGuoba.png',
+        ],
+    },
+    {
+        charaname: 'welt',
+        imgFiles: [
+            'Welt1.3Guoba.png',
+        ],
+    },
+    {
+        charaname: 'yanqing',
+        imgFiles: [
+            'YanqingGuobagraphic.png',
+        ],
+    },
+    {
+        charaname: 'yukong',
+        imgFiles: [
+            'YukongGuobagraphic.png',
         ],
     },
 ];
