915ec67c139aed3896a66fde13d23918c12bc39e
Mika C <makomitsuki@gmail.com>
2024-09-17 14:24:44 +0100

    zzz trial

---
+++ guobaguide-zzz.js
@@ -0,0 +1,16 @@
+zzzcharalist = [
+    {
+        charaname: 'Jane Doe',
+        imgFiles: [
+            'JANE.png',
+        ],
+    },
+    {
+        charaname: 'Qingyi',
+        imgFiles: [
+            'QINGYI.png',
+        ],
+    },
+];
+
+module.exports = { zzzcharalist }
